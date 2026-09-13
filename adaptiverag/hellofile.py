134f9668302efc7a1489dba2d85cdc265eca8677
shashank <40427468+shashank65@users.noreply.github.com>
2026-09-13 18:13:39 +0530

    month name

--- adaptiverag/hellofile.py
+++ adaptiverag/hellofile.py
@@ -5,3 +5,21 @@ def odd_even(x):
         return "even"
     else:
         return "odd"
+
+
+def month_name(month):
+    months = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+    return months.get(month, "Invalid month")
